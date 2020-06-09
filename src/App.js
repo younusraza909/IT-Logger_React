748c88699aef3a7709c546ea37c90ab6e4aaf53c
younusraza909 <younusraza909@gmail.com>
2020-06-09 08:15:02 -0700

    Made Log,LogItem,Navbar and Preloader with materialize-Css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+
+import Logs from "./components/logs/Logs";
+import SearchBar from "./components/layout/SearchBar";
 
 const App = () => {
   useEffect(() => {
     //this init materialize JS
     M.AutoInit();
   });
-  return <div className="App">My App</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ src/components/logs/Logs.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import LogItem from "./LogItem";
+import Preloader from "../layout/Preloader";
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // Use Effect Hook is called after component is renderd
+  useEffect(() => {
+    getLogs();
+    //eslint-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    //we added a proxy thats why we dont have to use localhost:5000 everytime
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Preloader />;
+  }
+
+  return (
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <h4 className="center">System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className="center">No Logs To Show</p>
+      ) : (
+        logs.map((log) => <LogItem key={log.id} log={log} />)
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
